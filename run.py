4911d0690836bd8aa85c3826de4ad79abd1009a8
Marvellousiscool <adebayoinioluwamarvellous1@gmail.com>
2024-09-02 02:08:17 +0100

    back to flask

--- run.py
+++ run.py
@@ -2,61 +2,49 @@ from flask import Flask
 from threading import Thread
 from highrise.__main__ import *
 import time
-import os
-from dotenv import load_dotenv
 
-# Load environment variables from .env file
-load_dotenv()
 
-# Get the values from the .env file
-port = int(os.getenv('PORT', 8080))  # Default to 8080 if not set
-room_id = os.getenv('ROOM_ID')
-bot_token = os.getenv('BOT_TOKEN')
+class WebServer():
 
-# Flask web server setup
-def create_app():
-    app = Flask(__name__)
+  def __init__(self):
+    self.app = Flask(__name__)
 
-    @app.route('/')
+    @self.app.route('/')
     def index() -> str:
-        return "Alive"
+      return "Alive"
 
-    return app
+  def run(self) -> None:
+    self.app.run(host='0.0.0.0', port=8080)
 
-# Create the app instance
-app = create_app()
+  def keep_alive(self):
+    t = Thread(target=self.run)
+    t.start()
 
-def run_flask():
-    print("Starting Flask server...")
-    app.run(host='0.0.0.0', port=port)
 
-# Bot setup and loop
-class RunBot:
-    def __init__(self) -> None:
-        print("Initializing bot...")
-        self.room_id = room_id
-        self.bot_token = bot_token
-        self.bot_file = "main"
-        self.bot_class = "Bot"
-        self.definitions = [
-            BotDefinition(
-                getattr(import_module(self.bot_file), self.bot_class)(),
-                self.room_id, self.bot_token)
-        ]
+class RunBot():
+  room_id = "6529060e1e8a5b090db8934b"
+  bot_token = "479ea14cd5d4f028845d29023ebe477a10d807825366304085db223118ac388e"
+  bot_file = "main"
+  bot_class = "Bot"
 
-    def run_loop(self) -> None:
-        print("Running bot loop...")
-        while True:
-            try:
-                arun(main(self.definitions))
-            except Exception as e:
-                print("Error: ", e)
-                time.sleep(5)
+  def __init__(self) -> None:
+    self.definitions = [
+        BotDefinition(
+            getattr(import_module(self.bot_file), self.bot_class)(),
+            self.room_id, self.bot_token)
+    ]  # More BotDefinition classes can be added to the definitions list
+
+  def run_loop(self) -> None:
+    while True:
+      try:
+        arun(main(self.definitions))
+
+      except Exception as e:
+        print("Error: ", e)
+        time.sleep(5)
+
 
 if __name__ == "__main__":
-    # Start the Flask web server in a separate thread
-    thread = Thread(target=run_flask)
-    thread.start()
+  WebServer().keep_alive()
 
-    # Run the bot loop
-    RunBot().run_loop()
+  RunBot().run_loop()
